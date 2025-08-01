63417f5bf7730af5e352c21e227c35077ad5f321
javilujann <jp.lujanromero@um.es>
2025-08-01 11:19:21 +0200

    Add renaming logic to export credentials

--- scripts/export-credentials-docker.ps1
+++ scripts/export-credentials-docker.ps1
@@ -13,8 +13,16 @@ docker-compose exec n8n n8n export:credentials --all --pretty --separate --outpu
 # Copiar los archivos del contenedor al host
 docker cp "n8n:/tmp/$OutputSubFolder/." "$fullPath"
 
-
 # Limpiar archivo temporal del contenedor
 docker-compose exec n8n rm -rf "/tmp/$OutputSubFolder"
+
+# Leer todos las credenciales del JSON y renombrarlos individualmente
+$credentials = Get-ChildItem -Path $fullPath -Filter "*.json"
+foreach ($file in $credentials) {
+    $credential = Get-Content $file.FullName -Raw | ConvertFrom-Json
+    $nameClean = $credential.name -replace '[<>:"/\\|?*]', '_'
+    $filename = "$nameClean.json"
+    Move-Item -Path $file.FullName -Destination (Join-Path $fullPath $filename) -Force
+}
 
 Write-Host "Credenciales exportadas a $fullPath"
--- scripts/export-credentials.ps1
+++ scripts/export-credentials.ps1
@@ -6,4 +6,13 @@ $outputFile = Join-Path $OutputFolder "credentials"
 
 n8n export:credentials --all --pretty --separate --output $outputFile
 
+# Leer todos las credenciales del JSON y renombrarlos individualmente
+$credentials = Get-ChildItem -Path $outputFile -Filter "*.json"
+foreach ($file in $credentials) {
+    $credential = Get-Content $file.FullName -Raw | ConvertFrom-Json
+    $nameClean = $credential.name -replace '[<>:"/\\|?*]', '_'
+    $filename = "$nameClean.json"
+    Move-Item -Path $file.FullName -Destination (Join-Path $outputFile $filename) -Force
+}
+
 Write-Host "Credenciales exportadas a $outputFile"
